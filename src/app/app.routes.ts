19d3d3a4ae508db842b076a19b27cb8e1cb301ae
Ihor Metko <i.metko@crosssoft.de>
2024-11-27 20:17:00 +0200

    Third commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { CalculatorComponent } from './calculator/calculator.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: '/calculator', pathMatch: 'full' },
-  { path: 'calculator', component: CalculatorComponent }
-];
